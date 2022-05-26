ad09af19751ff5ed0f838c046b0a1bb094606b23
SimoneCalf <simonecalf1998@gmail.com>
2022-05-26 12:40:38 +0200

    selectunit

--- salt_and_saturated_fat/src/toevoegen.js
+++ salt_and_saturated_fat/src/toevoegen.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Toevoegen = () => {
    
-
+    const foodData = [];  
 
     const foodRef = useRef(null);
     const unitRef = useRef(null);
@@ -44,18 +44,81 @@ const Toevoegen = () => {
         console.log(saltElement.value);
         const fatElement = fatRef.current;
         console.log(fatElement.value);
-        const addedFood = {}
-        addedFood["unit"] = unitElement.value
-        addedFood["salt"] = saltElement.value
-        addedFood["fat"] = fatElement.value
+
+        let unit = unitElement.value
+
+        if (unit === "100 gram") {
+            unit = "gram"
+        }
+        
+        
+        let addedFood = JSON.parse(localStorage.getItem(foodElement.value))
+
+        if (addedFood === null) {
+            addedFood = {
+                portie: {
+                    salt: null,
+                    fat: null
+                },
+                gram: {
+                    salt: null,
+                    fat: null
+                },
+                stuk: {
+                    salt: null,
+                    fat: null
+                }
+            }
+
+        }
+
+        addedFood[unit].salt = saltElement.value
+        addedFood[unit].fat = fatElement.value
+
+       
 
         localStorage.setItem(foodElement.value, JSON.stringify(addedFood));
-        const foodData = JSON.parse(localStorage.getItem(foodElement.value))
-        const items = localStorage.getItem(foodElement.value)
-        // console.log(foodData)
-        console.log(items)
-        return items
+        // const foodData = JSON.parse(localStorage.getItem(foodElement.value))
+    
+    
+    
+    
+    
+     // addToLocalStorage()
+    
+    
+    
+        // const addToLocalStorage = Object.entries(localStorage).map(entry => {  
+    //     const name = entry[0]
+    //     const info = JSON.parse(entry[1])
+        
+    //     // hier een loop die loopt over de array met keys en geparsde values van de local storage. 
+    //     // De keys(voedingsmiddelen) vergelijkt met de user input (foodElement.value). 
+    //     // Wanneer een key gelijk is aan de user input een andere key gebruiken.
+    
+    //     for (let i = 0; i < localStorage.length; i = i + 2) {
+    //         if (localStorage[i] === foodElement.value) {
+    //             foodElement.value = foodElement.value.concat('2')
+    //             localStorage.setItem(foodElement.value, JSON.stringify(addedFood));
+    //             const foodData = JSON.parse(localStorage.getItem(foodElement.value))
+    //         }
+    //         else {
+    //             localStorage.setItem(foodElement.value, JSON.stringify(addedFood));
+    //             const foodData = JSON.parse(localStorage.getItem(foodElement.value))
+    //         }
+
+    //     }
+           
+
+    
+    //     });
     }
+    
+    
+            
+        
+    
+    // }
 
     // iets van een for-loop die option tags returns met wat er in foodData is opgeslagen.
     // const getFood = () => {
@@ -118,7 +181,7 @@ const Toevoegen = () => {
                 </div>
                 <div>
                 <input 
-                    ref={saltRef}loca
+                    ref={saltRef}
                     type="text"
                     required
                     placeholder='12,6'
@@ -151,70 +214,177 @@ const SelectFood = () => {
     const unitRef = useRef(null);
     const selectedRef = useRef(null);
 
+    const [selected, setSelected] = useState('');
+
+    const handleSelectChange = (event) => {
+        setSelected(event.target.value);
+    }
+
 
     const options = Object.entries(localStorage).map(entry => {
         const info = JSON.parse(entry[1])
         const name = entry[0]
 
-        return (<option> voedingsmiddel: {name} eenheid: {info.unit} salt: {info.salt} verzadigd vet: {info.fat} </option>)
+        return (<option> voedingsmiddel: {name} eenheid per: {info.unit} salt: {info.salt} verzadigd vet: {info.fat} </option>)
+        //return (<option>{name}</option>)
 
     });
 
-    const SaltandFatAmount = () => {
-        let salt = 0
-        let fat = 0
+    const getSelectOptionsFoodElements = Object.entries(localStorage).map(entry => {
+        const name = entry[0]
 
-        if (selectedRef.info.unit == 'stuk' || selectedRef.info.unit == 'portie') {
-            salt = salt + info.salt * amountRef
-            fat = fat + info.fat * amountRef
-        }
-
-        if (selectedRef.info.unit == '100 gram') {
-            salt = salt + info.salt / 100 * amountRef
-        }
+        return(<option>{name}</option>)
         
-        
-        salt += selectedRef.info.salt
-        fat += selectedRef.info.fat
+    });
+    console.log(selected)
+    console.log(localStorage.getItem(selected))
+    // const selectedInfo = localStorage.getItem(selectedRef.current.value)
 
-        return(salt, fat)
+    const getSelectOptionsUnits = () => {
+        // const selectedRefElement = selectedRef.current;
+        // console.log(selectedRefElement.value)
+
+        //const []
+        const selectedInfo = JSON.parse(localStorage.getItem(selected))
+        console.log(selectedInfo)
+        // const selectedInfo = localStorage.getItem(selectedRefElement.value)
+        const selectOptions = []
+
+        if (selectedInfo.portie.salt !== null) {
+            const unitOption = <option>portie(s)</option>
+            selectOptions.push(unitOption)
+            //return(<option>"portie"</option>)
+        }
+        if (selectedInfo.stuk.salt !== null) {
+            const unitOption = <option>stuk(s)</option>
+            selectOptions.push(unitOption)
+            //return(<option>"stuk"</option>)
+        }
+        if (selectedInfo.gram.salt !== null) {
+            const unitOption = <option>gram</option>
+            selectOptions.push(unitOption)
+            //return(<option>"gram"</option>)
+        }
+        // else {
+        //     selectOptions.push(<option>"geen optie(s)"</option>)
+        // }
+        // selectOptions.map((option) => {
+        //      return option
+        // })
+
+        // console.log(selectOptions)
+        return selectOptions
+    }
+
+
+    const WatAanHetTesten = () => {
+        const item = selected
+        alert(item)
     }
 
 
 
+        // // banaan
+        // const name = entry[0]
+        // console.log(name)
+        // // info = [[portie]]:
+        // //     [[[salt]: [null],
+        // //     [fat]: [null]]]
+        // //
+        // // [gram]:
+        // //     [[salt]: [null],
+        // //     [fat]: [null]]
+        // // ,
+        // // [stuk]:
+        // //     [[salt]: [null],
+        // //     [fat]: [null]]]
+        // // }
+        // console.log(Object.entries(localStorage))
+        // const info = JSON.parse(entry[1])
+        // console.log(info)
+        // console.log(info.stuk.salt)
 
-    // options = Object.entries(localStorage).map(entry => {
-    //     JSON.parse(entry));
+
+
+        // // const optionsArray = []
+        // const selectOptions = []
+
+        
+
+        // if (name === selected && info.portie.salt !== null) {
+        //     const unitOption = "portie"
+        //     // return(<option>{info.unit}</option>)
+        //     //return(<option>{unitOption}</option>)
+        //     // optionsArray.concat({unitOption})
+        //     selectOptions.push({unitOption})
+        // }
+
+        // if (name === selected && info.stuk.salt !== null) {
+        //     const unitOption = "stuk"
+        //     // return(<option>{info.unit}</option>)
+        //     return(<option>{unitOption}</option>)
+        //     //optionsArray.concat({unitOption})
+        //     // selectOptions.concat({unitOption})
+
+        // }
+
+        // if (name === selected && info.gram.salt !== null){
+        //     const unitOption = "gram"
+        //     return(<option>{unitOption}</option>)
+        //     // optionsArray.concat({unitOption})
+        //     // selectOptions.concat({unitOption})
+        // }
+        // else {
+        //     // optionsArray.concat("geen optie(s)")
+        //     // selectOptions.concat("geen optie(s)")
+        //     return(<option>"geen optie(s)"</option>)
+        // }
+
+        
+        //const selectOptions = optionsArray.map((option) => <option>{option}</option>)
+        // return(selectOptions)
+        // console.log(selectOptions)
+
+
+
+
+
+        // if (name === selected && info.unit !== "100 gram") {
+        //     const unitOption = info.unit.concat("s")
+        //     // return(<option>{info.unit}</option>)
+        //     return(<option>{unitOption}</option>)
+        // }
+        // else if (name === selected && info.unit === "100 gram"){
+        //     const unitOptionGram = "gram"
+        //     return(<option>{unitOptionGram}</option>)
+        // }
+
+   // });
     
-    
-    // options.forEach(makeOptionArray)
+  
 
-    // function makeOptionArray(item) {
 
+
+    // const SaltandFatAmount = () => {
+    //     let salt = 0
+    //     let fat = 0
+
+    //     if (selectedRef.info.unit == 'stuk' || selectedRef.info.unit == 'portie') {
+    //         salt = salt + info.salt * amountRef
+    //         fat = fat + info.fat * amountRef
+    //     }
+
+    //     if (selectedRef.info.unit == '100 gram') {
+    //         salt = salt + info.salt / 100 * amountRef
+    //     }
+        
+        
+    //     salt += selectedRef.info.salt
+    //     fat += selectedRef.info.fat
+
+    //     return(salt, fat)
     // }
 
-
-    // optionTagArray = []
-
-    // makeOptionTags = () => {
-    //     // stringOptions =  Object.entries(localStorage).map(entry => {
-    //     //     console.log(entry);
-        
-    //     //options = JSON.parse(stringOptions)
-
-    //     options = Object.entries(localStorage).map(entry => {
-    //         JSON.parse(entry);
-
-    //     })
-
-        
-    
-    
-    
-    
-    // Object.entries(localStorage).map(entry => {
-    //     console.log(entry);
-    // })
 
 
     return (
@@ -222,19 +392,26 @@ const SelectFood = () => {
             <h2>Voedingsmiddel toevoegen aan wat u vandaag gaat eten</h2>
             <form>
                 <div>
-                <label>Selecteer wat u gaat eten</label>
-                <select ref={selectedRef}>
+                <label>Selecteer welk voedingsmiddel u gaat eten</label>
+                <select 
+                onChange={handleSelectChange}
+                value={selected}
+                ref={selectedRef}>
                 hier iets van een variabel toevoegen en die assignen aan een functie die key value pairs uit de local storage ophaalt
-                {options}
+                {/*{options}*/}
+                {getSelectOptionsFoodElements}
 
                 </select>
                 </div>
-                <label>Hoeveelheid die u gaat eten in</label>
+                {/*<label>Hoeveel gaat u hiervan gaat eten in</label>*/}
+                <label>Hoeveel</label>
                 <select ref={unitRef}>
-                    <option>gram</option>
+                {getSelectOptionsUnits()}
+                    {/* <option>gram</option>
                     <option>stuk(s)</option>
-                    <option>portie(s)</option>
+                    <option>portie(s)</option> */}
                 </select>
+                <label>gaat u hiervan gaat eten</label>
                 <div>
                     <input 
                         ref={amountRef}
@@ -244,7 +421,7 @@ const SelectFood = () => {
                     </input>
                 </div>
                 <div>
-                <button onClick={SaltandFatAmount}>toevoegen</button>
+                <button /*onClick={SaltandFatAmount}*/>toevoegen</button>
                 </div>
             </form>
         </>
